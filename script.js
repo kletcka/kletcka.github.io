bddaa86e8053bbd8698eda956004c7cf269d09f3
Error-me <63861193+Error-me@users.noreply.github.com>
2021-11-29 22:32:04 +0300

    c

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 let text = document.querySelectorAll('.ch');
-let headers = document.querySelectorAll('.hs');
-let nav = document.querySelectorAll('.ns');
-let bg = document.querySelector('.bg');
+let bg = document.querySelectorAll('.bg');
 let themeButton = document.querySelector('.theme-button');
 
 themeButton.onclick = function() {
     var index;
     for (index = 0; index < text.length; ++index) {
-        text[index].classList.toggle('light_theme');
-        text[index].classList.toggle('dark_theme');
+        text[index].classList.toggle('light');
+        text[index].classList.toggle('dark');
     }
-    for (index = 0; index < headers.length; ++index) {
-        headers[index].classList.toggle('header3_light');
-        headers[index].classList.toggle('header3_dark');
+    for (index = 0; index < bg.length; ++index) {
+        bg[index].classList.toggle('bg_light');
+        bg[index].classList.toggle('bg_dark');
     }
-    for (index = 0; index < nav.length; ++index) {
-        nav[index].classList.toggle('navigation_light');
-        nav[index].classList.toggle('navigation_dark');
-    }
-    bg.classList.toggle('bg_light');
-    bg.classList.toggle('bg_dark');
     
 
 };
